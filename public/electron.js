2fcca6cb97b66731041d059fae4bb622a91956c1
kkamara <kamaracomputers@gmail.com>
2026-08-22 20:16:10 +0100

    Fix YouTube live-stream player on production

--- public/electron.js
+++ public/electron.js
@@ -4,8 +4,30 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 
 const inProduction = app.isPackaged;
+const appReferrer = 'https://github.com/kkamara/the-kelvin-kamara-sound-app';
 
 let mainWindow;
+
+function configureYoutubeReferrer() {
+    electron.session.defaultSession.webRequest.onBeforeSendHeaders(
+        { urls: ['*://*.youtube.com/*', '*://*.youtube-nocookie.com/*'] },
+        (details, callback) => {
+            const referrerHeader = Object.keys(details.requestHeaders).find(
+                (header) => header.toLowerCase() === 'referer'
+            );
+            const referrer = referrerHeader && details.requestHeaders[referrerHeader];
+
+            if (!referrer || referrer.startsWith('file:')) {
+                if (referrerHeader) {
+                    delete details.requestHeaders[referrerHeader];
+                }
+                details.requestHeaders.Referer = appReferrer;
+            }
+
+            callback({ requestHeaders: details.requestHeaders });
+        }
+    );
+}
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -25,7 +47,12 @@ function createWindow() {
     mainWindow.on('closed', () => (mainWindow = null));
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+    if (inProduction) {
+        configureYoutubeReferrer();
+    }
+    createWindow();
+});
 
 process.on("uncaughtException", err => {
     if (false === inProduction) {
