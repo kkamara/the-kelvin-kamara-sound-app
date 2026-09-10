8ea264264e92253d269146b44ea10f9d64feb5aa
kkamara <kamaracomputers@gmail.com>
2026-09-10 16:13:07 +0100

    Update Apex Card whitenoise interval to 1.5 seconds

--- src/views/Dashboard/Default/SoundCard/ApexChartCard.js
+++ src/views/Dashboard/Default/SoundCard/ApexChartCard.js
@@ -24,7 +24,7 @@ const ApexChartCard = (props) => {
             if (audio && Number.isFinite(audio.duration) && audio.duration > 0) {
                 audio.currentTime = Math.random() * audio.duration;
             }
-        }, 1000);
+        }, 1500);
 
         return () => clearInterval(interval);
     }, []);
